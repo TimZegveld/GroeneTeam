241beaed5d739de3382a3d075c4e1e3b729dfe31
Rob-Post <rob@jem-id.nl>
2015-04-06 11:16:57 +0200

    Api project

---
+++ GroeneTeam.Api/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using JemId.Basis.BLL;
+
+namespace GroeneTeam.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            UpdateDatabase();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private static void UpdateDatabase()
+        {
+            AutoDbUpdater.UpdateDatabaseTables("JemId.Basis.DAL", "GroeneTeam.DAL");
+            new JemId.Basis.BLL.Updater().Update();
+        }
+    }
+}
