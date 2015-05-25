13b509ecae81d44ef1596f0a431e5394ba933747
Rob-Post <rob@jem-id.nl>
2015-05-25 11:33:18 +0200

    Web project poging 2

---
+++ GroeneTeam.Web/Controllers/LocatieController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GroeneTeam.BLL;
+
+namespace GroeneTeam.Web.Controllers
+{
+    public class LocatieController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var locaties = Locatie.GeefLijst();
+            return View(locaties);
+        }
+
+        public ActionResult Bewerk(int? id)
+        {
+            var locatie = new Locatie(id.GetValueOrDefault());
+            return View(locatie);
+        }
+
+        [HttpPost]
+        public ActionResult Bewerk(int? id, string txtNaam, string txtAdres, string txtPostcode, string txtPlaats, string txtLatitude, string txtLongitude)
+        {
+
+
+            //if (!ModelState.IsValid)
+            //    return RedirectToReferrer();
+
+            try
+            {
+                var locatie = new Locatie(id.GetValueOrDefault());
+                locatie.Opslaan(txtNaam, txtAdres, txtPostcode, txtPlaats);
+            }
+            catch (Exception ex) { }
+
+            //   return RedirectToIndex();
+            return new EmptyResult();
+        }
+    }
+}
