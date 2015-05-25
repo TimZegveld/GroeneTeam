2408fbb0025e6361eacb552ce3b448d6ca90e197
Rob-Post <rob@jem-id.nl>
2015-05-25 12:02:28 +0200

    Locatie index

---
+++ GroeneTeam.Web/Helpers/GroeneTeamHtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Web.Mvc.Html
+{
+    public static class GroeneTeamHtmlHelper
+    {
+        public static MvcHtmlString BootstrapIcon(this HtmlHelper html, string icoonNaam)
+        {
+            return new MvcHtmlString(string.Format("<span class=\"glyphicon glyphicon-{0}\"></span>", icoonNaam));
+        }
+    }
+}
