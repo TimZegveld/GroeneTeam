a9bf30b9b2da8170f39daffa96edb6a173d3e3e0
Rob-Post <rob@jem-id.nl>
2015-05-25 10:51:09 +0200

    Reference naar DAL en BLL

--- GroeneTeam.DAL/Locatie.cs
+++ GroeneTeam.DAL/Locatie.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JemId.Basis.DAL;
+﻿using JemId.Basis.DAL;
 
 namespace GroeneTeam.DAL
 {
